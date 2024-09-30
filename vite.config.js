ed8a05b3421a9581dcc86e468c0ff6d4a1a20f89
UyenPun <tranuyen2018@gmail.com>
2024-09-30 21:27:08 +0700

    Video 23. Cấu trúc dự án thực hành.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import dns from 'dns'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dns from "dns";
 // https://vitejs.dev/config/server-options.html#server-options
-dns.setDefaultResultOrder('verbatim')
+dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +10,4 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-})
+});
