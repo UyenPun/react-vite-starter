48d84828a1df305a04004b7a283d22ac5d80205a
UyenPun <tranuyen2018@gmail.com>
2024-09-30 21:43:01 +0700

    Video 27  Arrow Function.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
+// const tenHam = () => {};
+const App = () => {
   const [count, setCount] = useState(0);
 
   // Arrow Function:
-  // () => {};
-  const myFunction = (...args) => {
+  // const tenHam = () => {};
+  const myFunction = () => {
     console.log("hello Babe");
   };
 
@@ -50,6 +51,6 @@ function App() {
       </p>
     </>
   );
-}
+};
 
 export default App;
